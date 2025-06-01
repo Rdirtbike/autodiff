0cff19714814edd730610e8fce55ab6caadf75cc
Ryan Meredith <ryan@ryanmeredith.com>
2025-06-01 18:20:41 -0400

    Initial attempt at mutable differential vectors

--- src/Data/Autodiff/DMVec.hs
+++ src/Data/Autodiff/DMVec.hs
@@ -1,14 +1,30 @@
-module Data.Autodiff.DMVec (DMVec) where
+{-# LANGUAGE TypeFamilies #-}
 
-import Data.Vector.Generic.Mutable (MVector (..))
+module Data.Autodiff.DMVec (DMVec (..)) where
 
-data DMVec v s a
+import Control.Monad.ST
+import Data.Autodiff.Internal
+import Data.Autodiff.VectorSpace
+import Data.Functor.Invariant
+import Data.Kind
+import Data.STRef
+import Data.Vector.Generic (Mutable, Vector, replicate, snoc, unsafeIndex, unsafeUpd, (++))
+import Data.Vector.Generic qualified as I
+import Data.Vector.Generic.Mutable hiding (replicate)
+import Prelude hiding (replicate, (++))
 
-instance MVector (DMVec v) a where
-  basicLength x = case x of {}
-  basicUnsafeSlice _ _ x = case x of {}
-  basicOverlaps x = case x of {}
-  basicUnsafeNew _ = pure $ error "Cannot create a mutable differential vector, no implementation"
-  basicInitialize x = case x of {}
-  basicUnsafeRead x = case x of {}
-  basicUnsafeWrite x = case x of {}
+data family DMVec :: (Type -> Type) -> Type -> Type -> Type
+
+data instance DMVec v s (D q m a) = MkMV (Mutable v s a) (ST s (STRef s (m (v a))))
+
+instance (Vector v a, Invariant m, Num a, VectorSpace (m (v a))) => MVector (DMVec v) (D q m a) where
+  basicLength (MkMV v _) = basicLength v
+  basicUnsafeSlice i n (MkMV v mv) = MkMV (basicUnsafeSlice i n v) $ mv >>= \v' -> v' <$ modifySTRef v' (invmap (I.basicUnsafeSlice i n) (replicate i 0 ++))
+  basicOverlaps (MkMV v _) (MkMV w _) = basicOverlaps v w
+  basicUnsafeNew n = flip MkMV (newSTRef zero) <$> basicUnsafeNew n
+  basicInitialize (MkMV v _) = basicInitialize v
+  basicUnsafeRead (MkMV v mv) i = liftA2 MkD (basicUnsafeRead v i) $ invmap (`unsafeIndex` i) (snoc $ replicate i 0) <$> (mv >>= readSTRef)
+  basicUnsafeWrite (MkMV v mv) i (MkD x x') =
+    basicUnsafeWrite v i x
+      >> mv
+      >>= flip modifySTRef (\v' -> invmap (`unsafeUpd` [(i, 0)]) (`unsafeUpd` [(i, 0)]) v' .+ invmap (snoc $ replicate i 0) (`unsafeIndex` i) x')
--- src/Data/Autodiff/DVec.hs
+++ src/Data/Autodiff/DVec.hs
@@ -2,10 +2,12 @@
 
 module Data.Autodiff.DVec (DVec (..)) where
 
-import Data.Autodiff.DMVec (DMVec)
+import Data.Autodiff.DMVec (DMVec (MkMV))
 import Data.Autodiff.Internal (D (..))
+import Data.Autodiff.VectorSpace (VectorSpace)
 import Data.Functor.Invariant (Invariant (..))
 import Data.Kind (Type)
+import Data.STRef (newSTRef, readSTRef)
 import Data.Vector.Fusion.Util (Box (..))
 import Data.Vector.Generic (Mutable, Vector (..), replicate, snoc, (++))
 import Prelude hiding (replicate, (++))
@@ -16,9 +18,9 @@ newtype instance DVec v (D s m a) = MkV {getV :: D s m (v a)}
 
 type instance Mutable (DVec v) = DMVec v
 
-instance (Vector v a, Invariant m, Num a) => Vector (DVec v) (D s m a) where
-  basicUnsafeFreeze x = case x of {}
-  basicUnsafeThaw _ = pure $ error "No mutable differential vector implementation"
+instance (Vector v a, Invariant m, Num a, VectorSpace (m (v a))) => Vector (DVec v) (D s m a) where
+  basicUnsafeFreeze (MkMV x x') = MkV <$> liftA2 MkD (basicUnsafeFreeze x) (x' >>= readSTRef)
+  basicUnsafeThaw (MkV (MkD x x')) = flip MkMV (newSTRef x') <$> basicUnsafeThaw x
   basicLength (MkV (MkD x _)) = basicLength x
   basicUnsafeSlice i n (MkV (MkD x x')) = MkV $ MkD (basicUnsafeSlice i n x) $ invmap (basicUnsafeSlice i n) (replicate i 0 ++) x'
   basicUnsafeIndexM (MkV (MkD x x')) i = liftA2 MkD (basicUnsafeIndexM x i) $ Box $ invmap (unBox . (`basicUnsafeIndexM` i)) (snoc $ replicate i 0) x'
