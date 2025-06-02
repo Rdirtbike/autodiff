4d7824989e394a7dec47e6fb995f6927f6473800
Ryan Meredith <ryan@ryanmeredith.com>
2025-06-02 10:02:41 -0400

    Fixed slice issue... with unsafe functions :(

--- src/Data/Autodiff/DMVec.hs
+++ src/Data/Autodiff/DMVec.hs
@@ -2,7 +2,7 @@
 
 module Data.Autodiff.DMVec (DMVec (..)) where
 
-import Control.Monad.ST (ST)
+import Control.Monad.ST.Unsafe
 import Data.Autodiff.Internal (D (..))
 import Data.Autodiff.VectorSpace (VectorSpace (zero, (.+)))
 import Data.Functor.Invariant (Invariant (..))
@@ -11,22 +11,20 @@ import Data.Maybe (fromMaybe)
 import Data.STRef (STRef, modifySTRef', newSTRef, readSTRef)
 import Data.Vector.Generic (Mutable, Vector, drop, length, replicate, snoc, take, unsafeUpd, (!?), (++))
 import Data.Vector.Generic.Mutable (MVector (..))
+import System.IO.Unsafe
 import Prelude hiding (drop, length, read, replicate, take, (++))
 
 data family DMVec :: (Type -> Type) -> Type -> Type -> Type
 
-data instance DMVec v s (D q m a) = MkMV (Mutable v s a) (ST s (STRef s (m (v a))))
+data instance DMVec v s (D q m a) = MkMV (Mutable v s a) (STRef s (m (v a)))
 
 instance (Vector v a, Invariant m, Num a, VectorSpace (m (v a))) => MVector (DMVec v) (D q m a) where
   basicLength (MkMV v _) = basicLength v
-  basicUnsafeSlice i n (MkMV v mv) = MkMV (basicUnsafeSlice i n v) $ mv >>= \v' -> v' <$ modifySTRef' v' (invmap (take n . drop i) (replicate i 0 ++))
+  basicUnsafeSlice i n (MkMV v vr) = MkMV (basicUnsafeSlice i n v) $ unsafePerformIO $ unsafeSTToIO $ vr <$ modifySTRef' vr (invmap (take n . drop i) (replicate i 0 ++))
   basicOverlaps (MkMV v _) (MkMV w _) = basicOverlaps v w
-  basicUnsafeNew n = liftA2 MkMV (basicUnsafeNew n) $ pure <$> newSTRef zero
+  basicUnsafeNew n = MkMV <$> basicUnsafeNew n <*> newSTRef zero
   basicInitialize (MkMV v _) = basicInitialize v
-  basicUnsafeRead (MkMV v mv) i = liftA2 MkD (basicUnsafeRead v i) $ invmap (fromMaybe 0 . (!? i)) (snoc $ replicate i 0) <$> (mv >>= readSTRef)
-  basicUnsafeWrite (MkMV v mv) i (MkD x x') =
-    basicUnsafeWrite v i x
-      >> mv
-      >>= flip modifySTRef' (\v' -> invmap clearI clearI v' .+ invmap (snoc $ replicate i 0) (fromMaybe 0 . (!? i)) x')
+  basicUnsafeRead (MkMV v vr) i = MkD <$> basicUnsafeRead v i <*> (invmap (fromMaybe 0 . (!? i)) (snoc $ replicate i 0) <$> readSTRef vr)
+  basicUnsafeWrite (MkMV v vr) i (MkD x x') = basicUnsafeWrite v i x *> modifySTRef' vr (\v' -> invmap clearI clearI v' .+ invmap (snoc $ replicate i 0) (fromMaybe 0 . (!? i)) x')
     where
       clearI y = if i < length y then unsafeUpd y [(i, 0)] else y
--- src/Data/Autodiff/DVec.hs
+++ src/Data/Autodiff/DVec.hs
@@ -9,7 +9,6 @@ import Data.Functor.Invariant (Invariant (..))
 import Data.Kind (Type)
 import Data.Maybe (fromMaybe)
 import Data.STRef (newSTRef, readSTRef)
-import Data.Vector.Fusion.Util (Box (..))
 import Data.Vector.Generic (Mutable, Vector (..), drop, replicate, snoc, take, (!?), (++))
 import Prelude hiding (drop, replicate, take, (++))
 
@@ -20,9 +19,9 @@ newtype instance DVec v (D s m a) = MkV {getV :: D s m (v a)}
 type instance Mutable (DVec v) = DMVec v
 
 instance (Vector v a, Invariant m, Num a, VectorSpace (m (v a))) => Vector (DVec v) (D s m a) where
-  basicUnsafeFreeze (MkMV x x') = MkV <$> liftA2 MkD (basicUnsafeFreeze x) (x' >>= readSTRef)
-  basicUnsafeThaw (MkV (MkD x x')) = liftA2 MkMV (basicUnsafeThaw x) $ pure <$> newSTRef x'
+  basicUnsafeFreeze (MkMV x x') = MkV <$> (MkD <$> basicUnsafeFreeze x <*> readSTRef x')
+  basicUnsafeThaw (MkV (MkD x x')) = MkMV <$> basicUnsafeThaw x <*> newSTRef x'
   basicLength (MkV (MkD x _)) = basicLength x
-  basicUnsafeSlice i n (MkV (MkD x x')) = MkV $ MkD (basicUnsafeSlice i n x) $ invmap (take n . drop i) (replicate i 0 ++) x'
-  basicUnsafeIndexM (MkV (MkD x x')) i = liftA2 MkD (basicUnsafeIndexM x i) $ Box $ invmap (fromMaybe 0 . (!? i)) (snoc $ replicate i 0) x'
+  basicUnsafeSlice i n (MkV (MkD x x')) = MkV $ MkD (basicUnsafeSlice i n x) (invmap (take n . drop i) (replicate i 0 ++) x')
+  basicUnsafeIndexM (MkV (MkD x x')) i = MkD <$> basicUnsafeIndexM x i <*> pure (invmap (fromMaybe 0 . (!? i)) (snoc $ replicate i 0) x')
   elemseq (MkV (MkD v _)) (MkD x _) = elemseq v x
