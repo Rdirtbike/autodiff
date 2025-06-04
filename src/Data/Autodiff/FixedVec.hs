85a7b853ecf0c9c5101e5aa66a43b79d321a5d78
Ryan Meredith <ryan@ryanmeredith.com>
2025-06-04 11:45:29 -0400

    Cleaned up implementation

--- src/Data/Autodiff/FixedVec.hs
+++ src/Data/Autodiff/FixedVec.hs
@@ -7,13 +7,39 @@ module Data.Autodiff.FixedVec (V) where
 import Data.Autodiff.Mode (HasBasis (..))
 import Data.Autodiff.VectorSpace (InnerSpace (..), VectorSpace (..))
 import Data.Foldable (traverse_)
-import Data.List qualified as L
-import Data.Vector.Generic qualified as G
-import Data.Vector.Generic.Mutable qualified as M
-import Data.Vector.Unboxed hiding (toList)
+import Data.Vector.Generic (basicUnsafeCopy, basicUnsafeFreeze, elemseq)
+import Data.Vector.Generic qualified as G (Vector (..))
+import Data.Vector.Generic.Mutable
+  ( basicClear,
+    basicInitialize,
+    basicLength,
+    basicOverlaps,
+    basicUnsafeGrow,
+    basicUnsafeMove,
+    basicUnsafeNew,
+    basicUnsafeRead,
+    basicUnsafeSlice,
+    basicUnsafeWrite,
+  )
+import Data.Vector.Generic.Mutable qualified as M (MVector (..), length, new, write)
+import Data.Vector.Unboxed
+  ( MVector,
+    Unbox,
+    Vector,
+    create,
+    freeze,
+    length,
+    map,
+    replicate,
+    sum,
+    unsafeSlice,
+    unsafeThaw,
+    unsafeUpd,
+    zipWith,
+  )
 import GHC.IsList (IsList (..))
 import GHC.TypeNats (KnownNat (..), natVal)
-import Prelude hiding (concat, map, replicate, sum, zipWith)
+import Prelude hiding (concat, length, map, replicate, sum, zipWith)
 
 newtype V n a = MkV (Vector a) deriving (Show, Eq, Ord)
 
@@ -37,35 +63,33 @@ instance (KnownNat n, Unbox a, Num a) => HasBasis (V n) (V n a) where
 
 instance (KnownNat n, Unbox a, VectorSpace a) => IsList (V n a) where
   type Item (V n a) = a
-  fromList xs = let n = knownNatAsInt @n in MkV $ replicate n zero `unsafeUpd` L.zip [0 ..] (L.take n xs)
+  fromList xs = let n = knownNatAsInt @n in MkV $ replicate n zero `unsafeUpd` zip [0 ..] (take n xs)
   toList (MkV v) = toList v
 
 newtype instance MVector s (V n a) = MkM (MVector s a)
 
 instance (KnownNat n, Unbox a) => M.MVector MVector (V n a) where
   basicLength (MkM v) = M.length v `div` knownNatAsInt @n
-  basicUnsafeSlice i n (MkM v) = let m = knownNatAsInt @n in MkM $ M.basicUnsafeSlice (i * m) (n * m) v
-  basicOverlaps (MkM v) (MkM w) = M.basicOverlaps v w
-  basicUnsafeNew n = MkM <$> M.basicUnsafeNew (n * knownNatAsInt @n)
-  basicInitialize (MkM v) = M.basicInitialize v
-  basicUnsafeReplicate n (MkV v) = MkM <$> thaw (concat $ L.replicate n v)
-  basicUnsafeRead (MkM v) i = let n = knownNatAsInt @n in MkV <$> freeze (M.basicUnsafeSlice (i * n) n v)
-  basicUnsafeWrite (MkM v) i (MkV w) = let n = knownNatAsInt @n in G.basicUnsafeCopy (M.basicUnsafeSlice (i * n) n v) w
-  basicClear (MkM v) = M.basicClear v
-  basicSet x@(MkM v) (MkV w) = G.basicUnsafeCopy v $ concat $ L.replicate (M.length x) w
+  basicUnsafeSlice i n (MkM v) = let m = knownNatAsInt @n in MkM $ basicUnsafeSlice (i * m) (n * m) v
+  basicOverlaps (MkM v) (MkM w) = basicOverlaps v w
+  basicUnsafeNew n = MkM <$> basicUnsafeNew (n * knownNatAsInt @n)
+  basicInitialize (MkM v) = basicInitialize v
+  basicUnsafeRead (MkM v) i = let n = knownNatAsInt @n in MkV <$> freeze (basicUnsafeSlice (i * n) n v)
+  basicUnsafeWrite (MkM v) i (MkV w) = let n = knownNatAsInt @n in basicUnsafeCopy (basicUnsafeSlice (i * n) n v) w
+  basicClear (MkM v) = basicClear v
   basicUnsafeCopy (MkM v) (MkM w) = M.basicUnsafeCopy v w
-  basicUnsafeMove (MkM v) (MkM w) = M.basicUnsafeMove v w
-  basicUnsafeGrow (MkM v) n = MkM <$> M.basicUnsafeGrow v (n * knownNatAsInt @n)
+  basicUnsafeMove (MkM v) (MkM w) = basicUnsafeMove v w
+  basicUnsafeGrow (MkM v) n = MkM <$> basicUnsafeGrow v (n * knownNatAsInt @n)
 
 newtype instance Vector (V n a) = MkVV (Vector a)
 
 instance (KnownNat n, Unbox a) => G.Vector Vector (V n a) where
-  basicUnsafeFreeze (MkM v) = MkVV <$> G.basicUnsafeFreeze v
-  basicUnsafeThaw (MkVV v) = MkM <$> G.basicUnsafeThaw v
-  basicLength (MkVV v) = G.length v `div` knownNatAsInt @n
-  basicUnsafeSlice i n (MkVV v) = let m = knownNatAsInt @n in MkVV $ G.basicUnsafeSlice (i * m) (n * m) v
-  basicUnsafeIndexM (MkVV v) i = let n = knownNatAsInt @n in pure $ MkV $ G.basicUnsafeSlice (i * n) n v
-  basicUnsafeCopy (MkM v) (MkVV w) = G.basicUnsafeCopy v w
-  elemseq _ = G.elemseq @Vector undefined
+  basicUnsafeFreeze (MkM v) = MkVV <$> basicUnsafeFreeze v
+  basicUnsafeThaw (MkVV v) = MkM <$> unsafeThaw v
+  basicLength (MkVV v) = length v `div` knownNatAsInt @n
+  basicUnsafeSlice i n (MkVV v) = let m = knownNatAsInt @n in MkVV $ unsafeSlice (i * m) (n * m) v
+  basicUnsafeIndexM (MkVV v) i = let n = knownNatAsInt @n in pure $ MkV $ unsafeSlice (i * n) n v
+  basicUnsafeCopy (MkM v) (MkVV w) = basicUnsafeCopy v w
+  elemseq _ = elemseq @Vector undefined
 
 instance (KnownNat n, Unbox a) => Unbox (V n a)
