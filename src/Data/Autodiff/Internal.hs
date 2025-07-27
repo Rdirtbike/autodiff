604734580ca5297cb0f3dedab2747a6e4a8c7a0f
Ryan Meredith <ryan@ryanmeredith.com>
2025-07-27 12:08:22 -0400

    New type to symplify constants

--- src/Data/Autodiff/Internal.hs
+++ src/Data/Autodiff/Internal.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE UnboxedTuples #-}
 
-module Data.Autodiff.Internal where
+module Data.Autodiff.Internal (D, autodiff) where
 
 import Data.IORef
 import GHC.Exts
@@ -16,39 +16,35 @@ newTag = IO $ \s -> case newPromptTag# s of
 prompt :: Tag a -> IO a -> IO a
 prompt (MkT t) (IO f) = IO $ \s -> prompt# t f s
 
-control :: Tag a -> ((b -> IO a) -> IO a) -> IO b
-control (MkT t) f = IO $ \s -> control0# t (\g -> unIO $ f $ \x -> IO $ g (# ,x #)) s
+control0 :: Tag a -> ((b -> IO a) -> IO a) -> IO b
+control0 (MkT t) f = IO $ \s -> control0# t (\g -> unIO $ f $ \x -> IO $ g (# ,x #)) s
 
-data D a = MkD (Tag ()) a (IO (IORef a)) | MkC a
+data D a = MkD a (Tag () -> IO (IORef a))
 
 autodiff :: (Num a, Num b) => (D a -> D b) -> a -> IO (b, a)
 autodiff f x = do
   r <- newIORef 0
   t <- newTag
-  case f $ MkD t x $ pure r of
-    MkC y -> pure (y, 0)
-    MkD _ y yd -> do
-      prompt t $ yd >>= (`writeIORef` 1)
+  case f $ MkD x $ \_ -> pure r of
+    MkD y yd -> do
+      prompt t $ yd t >>= (`writeIORef` 1)
       y' <- readIORef r
       pure (y, y')
 
 lift :: Num a => (a -> a) -> (a -> a) -> D a -> D a
-lift f _ (MkC x) = MkC $ f x
-lift f f' (MkD t x xd) = MkD t (f x) $ do
-  x' <- xd
-  control t $ \k -> do
+lift f f' (MkD x xd) = MkD (f x) $ \t -> do
+  x' <- xd t
+  control0 t $ \k -> do
     r <- newIORef 0
     prompt t $ k r
     y' <- readIORef r
     modifyIORef' x' (+ f' x * y')
 
 lift2 :: Num a => (a -> a -> a) -> (a -> a -> a) -> (a -> a -> a) -> D a -> D a -> D a
-lift2 f _ f2' (MkC x) y = lift (f x) (f2' x) y
-lift2 f f1' _ x (MkC y) = lift (`f` y) (`f1'` y) x
-lift2 f f1' f2' (MkD t x xd) (MkD _ y yd) = MkD t (f x y) $ do
-  x' <- xd
-  y' <- yd
-  control t $ \k -> do
+lift2 f f1' f2' (MkD x xd) (MkD y yd) = MkD (f x y) $ \t -> do
+  x' <- xd t
+  y' <- yd t
+  control0 t $ \k -> do
     r <- newIORef 0
     prompt t $ k r
     z' <- readIORef r
@@ -61,4 +57,4 @@ instance Num a => Num (D a) where
   negate = lift negate (const $ -1)
   abs = lift abs signum
   signum = lift abs (const 0)
-  fromInteger n = MkC $ fromInteger n
+  fromInteger n = MkD (fromInteger n) $ \_ -> newIORef 0
