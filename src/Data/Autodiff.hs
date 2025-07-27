4f576914279ce0b9be01cfb408b66965bd7fe022
Ryan Meredith <ryan@ryanmeredith.com>
2025-07-27 12:08:52 -0400

    Removed outdated modules

--- src/Data/Autodiff.hs
+++ src/Data/Autodiff.hs
@@ -1,24 +1,60 @@
-module Data.Autodiff (D, autodiff, gradient, asConst, asConstD) where
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
 
-import Data.Autodiff.DVec (DVec (MkV))
-import Data.Autodiff.Internal (D (..))
-import Data.Autodiff.Mode (Mode (..), ReverseMode, getGradient)
-import Data.Autodiff.VectorSpace (VectorSpace (zero))
-import Data.Functor.Contravariant (Op)
-import Data.Vector.Unboxed qualified as U
+module Data.Autodiff (D, autodiff) where
 
-{-# SPECIALIZE autodiff :: (forall s. D s (Op (U.Vector Double)) (U.Vector Double) -> D s (Op (U.Vector Double)) Double) -> U.Vector Double -> (Double, Op (U.Vector Double) Double) #-}
-autodiff :: (Mode m, Start m a) => (forall s. D s m a -> D s m b) -> a -> (b, m b)
-autodiff f x = case f (MkD x start) of
-  MkD y f' -> (y, f')
+import Data.IORef
+import GHC.Exts
+import GHC.IO
 
-{-# SPECIALIZE gradient :: (forall s. DVec U.Vector (D s (Op (U.Vector Double)) Double) -> D s (Op (U.Vector Double)) Double) -> U.Vector Double -> (Double, U.Vector Double) #-}
-gradient :: (VectorSpace (v a), Num b) => (forall s. DVec v (D s (ReverseMode (v a)) a) -> D s (ReverseMode (v a)) b) -> v a -> (b, v a)
-gradient f x = case f (MkV (MkD x start)) of
-  MkD y f' -> (y, getGradient f')
+data Tag a = MkT (PromptTag# a)
 
-asConst :: (Real a, Fractional b) => D s m a -> b
-asConst (MkD x _) = realToFrac x
+newTag :: IO (Tag a)
+newTag = IO $ \s -> case newPromptTag# s of
+  (# s', t #) -> (# s', MkT t #)
 
-asConstD :: VectorSpace (m a) => D s m a -> D s' m a
-asConstD (MkD x _) = MkD x zero
+prompt :: Tag a -> IO a -> IO a
+prompt (MkT t) (IO f) = IO $ \s -> prompt# t f s
+
+control0 :: Tag a -> ((b -> IO a) -> IO a) -> IO b
+control0 (MkT t) f = IO $ \s -> control0# t (\g -> unIO $ f $ \x -> IO $ g (# ,x #)) s
+
+data D a = MkD a (Tag () -> IO (IORef a))
+
+autodiff :: (Num a, Num b) => (D a -> D b) -> a -> IO (b, a)
+autodiff f x = do
+  r <- newIORef 0
+  t <- newTag
+  case f $ MkD x $ \_ -> pure r of
+    MkD y yd -> do
+      prompt t $ yd t >>= (`writeIORef` 1)
+      y' <- readIORef r
+      pure (y, y')
+
+lift :: Num a => (a -> a) -> (a -> a) -> D a -> D a
+lift f f' (MkD x xd) = MkD (f x) $ \t -> do
+  x' <- xd t
+  control0 t $ \k -> do
+    r <- newIORef 0
+    prompt t $ k r
+    y' <- readIORef r
+    modifyIORef' x' (+ f' x * y')
+
+lift2 :: Num a => (a -> a -> a) -> (a -> a -> a) -> (a -> a -> a) -> D a -> D a -> D a
+lift2 f f1' f2' (MkD x xd) (MkD y yd) = MkD (f x y) $ \t -> do
+  x' <- xd t
+  y' <- yd t
+  control0 t $ \k -> do
+    r <- newIORef 0
+    prompt t $ k r
+    z' <- readIORef r
+    modifyIORef' x' (+ f1' x y * z')
+    modifyIORef' y' (+ f2' x y * z')
+
+instance Num a => Num (D a) where
+  (+) = lift2 (+) (\_ _ -> 1) (\_ _ -> 1)
+  (*) = lift2 (*) (\_ y -> y) const
+  negate = lift negate (const $ -1)
+  abs = lift abs signum
+  signum = lift abs (const 0)
+  fromInteger n = MkD (fromInteger n) $ \_ -> newIORef 0
