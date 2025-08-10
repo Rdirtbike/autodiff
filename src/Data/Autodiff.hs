6a37247e7129b520f09c6298b1835d196586f524
Ryan Meredith <ryan@ryanmeredith.com>
2025-08-10 18:17:56 -0400

    New implementation

--- src/Data/Autodiff.hs
+++ src/Data/Autodiff.hs
@@ -1,73 +1,68 @@
-{-# LANGUAGE Strict #-}
-
 module Data.Autodiff (D, autodiff) where
 
 import Control.Monad.ST (ST)
 import Data.Autodiff.MNum (MFloating, MFractional, MNum)
 import Data.Autodiff.MNum qualified
-import Data.Primitive.ByteArray (MutableByteArray, fillByteArray, newByteArray, readByteArray, writeByteArray)
-import Data.Primitive.Types (Prim)
-import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef)
+import Data.STRef (STRef, modifySTRef', newSTRef, readSTRef, writeSTRef)
 
-newtype M s a = MkM
-  {runM :: forall r. STRef s Int -> (a -> ST s (r, MutableByteArray s)) -> ST s (r, MutableByteArray s)}
+newtype M s a = MkM {runM :: forall r. (a -> ST s r) -> ST s r}
   deriving Functor
 
 instance Applicative (M s) where
-  pure x = MkM $ const ($ x)
-  MkM f <*> MkM x = MkM $ \n k -> f n $ \g -> x n (k . g)
+  pure x = MkM ($ x)
+  MkM f <*> MkM x = MkM $ \k -> f $ \g -> x (k . g)
   x *> y = x >>= const y
 
 instance Monad (M s) where
-  MkM m >>= f = MkM $ \n k -> m n $ \x -> runM (f x) n k
+  MkM m >>= f = MkM $ \k -> m $ \x -> runM (f x) k
 
-data D s a = MkD a Int
+data D s a = MkD !a {-# UNPACK #-} !(STRef s a)
 
 {-# INLINEABLE autodiff #-}
-autodiff :: forall a b s. (Prim a, Num b, Prim b) => (D s a -> M s (D s b)) -> a -> ST s (b, a)
+autodiff :: (Num a, Num b) => (D s a -> M s (D s b)) -> a -> ST s (b, a)
 autodiff f x = do
-  nr <- newSTRef 1
-  (y, a) <- runM (f $ MkD x 0) nr $ \(MkD y i) -> do
-    n <- readSTRef nr
-    a <- newByteArray $ n * 8
-    fillByteArray a 0 (n * 8) 0
-    writeByteArray @b a i 1
-    pure (y, a)
-  y' <- readByteArray a 0
+  r <- newSTRef 0
+  y <- runM (f $ MkD x r) $
+    \(MkD y y') -> y <$ writeSTRef y' 1
+  y' <- readSTRef r
   pure (y, y')
 
 {-# INLINE lift #-}
 lift :: Num a => a -> M s (D s a)
-lift x = MkM $ \nr k -> do
-  i <- readSTRef nr
-  writeSTRef nr $! i + 1
-  k $! MkD x i
+lift x = MkM $ \k -> newSTRef 0 >>= k . MkD x
 
 {-# INLINE lift1 #-}
-lift1 :: (Num a, Prim a) => (a -> a) -> (a -> a -> a -> a) -> D s a -> M s (D s a)
-lift1 f f' (MkD x ix) = MkM $ \nr k -> do
-  iy <- readSTRef nr
-  writeSTRef nr $! iy + 1
-  (y, a) <- k $! MkD (f x) iy
-  y' <- readByteArray a iy
-  x' <- readByteArray a ix
-  writeByteArray a ix $! f' x y' x'
-  pure (y, a)
+lift1 ::
+  Num a =>
+  (a -> a) ->
+  (a -> a -> a -> a) ->
+  D s a ->
+  M s (D s a)
+lift1 f f' (MkD x x') = MkM $ \k -> do
+  r <- newSTRef 0
+  y <- k $ MkD (f x) r
+  y' <- readSTRef r
+  modifySTRef' x' $ f' x y'
+  pure y
 
 {-# INLINE lift2 #-}
-lift2 :: (Num a, Prim a) => (a -> a -> a) -> (a -> a -> a -> a -> a) -> (a -> a -> a -> a -> a) -> D s a -> D s a -> M s (D s a)
-lift2 f f1' f2' (MkD x ix) (MkD y iy) = MkM $ \nr k -> do
-  iz <- readSTRef nr
-  writeSTRef nr $! iz + 1
-  (z, a) <- k $! MkD (f x y) iz
-  z' <- readByteArray a iz
-  x' <- readByteArray a ix
-  writeByteArray a ix $! f1' x y z' x'
-  y' <- readByteArray a iy
-  writeByteArray a iy $! f2' x y z' y'
-  pure (z, a)
+lift2 ::
+  Num a =>
+  (a -> a -> a) ->
+  (a -> a -> a -> a -> a) ->
+  (a -> a -> a -> a -> a) ->
+  D s a ->
+  D s a ->
+  M s (D s a)
+lift2 f f1' f2' (MkD x x') (MkD y y') = MkM $ \k -> do
+  r <- newSTRef 0
+  z <- k $ MkD (f x y) r
+  z' <- readSTRef r
+  modifySTRef' x' $ f1' x y z'
+  modifySTRef' y' $ f2' x y z'
+  pure z
 
-instance (Num a, Prim a) => MNum (M s) (D s a) where
+instance Num a => MNum (M s) (D s a) where
   (+) = lift2 (+) (\_ _ z' -> (+ z')) (\_ _ z' -> (+ z'))
   (*) = lift2 (*) (\_ y z' -> (+ z' * y)) (\x _ z' -> (+ z' * x))
   (-) = lift2 (-) (\_ _ z' -> (+ z')) (\_ _ z' -> (- z'))
@@ -76,18 +71,26 @@ instance (Num a, Prim a) => MNum (M s) (D s a) where
   signum (MkD x _) = lift $ signum x
   fromInteger n = lift $ fromInteger n
 
-instance (Fractional a, Prim a) => MFractional (M s) (D s a) where
+instance Fractional a => MFractional (M s) (D s a) where
   (/) = lift2 (/) (\_ y z' -> (+ z' / y)) (\x y z' -> (- z' * x / (y * y)))
   recip = lift1 recip $ \x y' -> (- y' / (x * x))
   fromRational x = lift $ fromRational x
 
-instance (Floating a, Prim a) => MFloating (M s) (D s a) where
+instance Floating a => MFloating (M s) (D s a) where
   pi = lift pi
   exp = lift1 exp $ \x y' -> (+ y' * exp x)
   log = lift1 log $ \x y' -> (+ y' / x)
   sqrt = lift1 sqrt $ \x y' -> (- y' / sqrt x)
-  (**) = lift2 (**) (\x y z' -> (+ z' * y * x ** (y - 1))) (\x y z' -> (+ z' * log x * x ** y))
-  logBase = lift2 logBase (\x y z' -> (- z' * log y / (x * log x ^ (2 :: Int)))) (\x y z' -> (+ z' / (y * log x)))
+  (**) =
+    lift2
+      (**)
+      (\x y z' -> (+ z' * y * x ** (y - 1)))
+      (\x y z' -> (+ z' * log x * x ** y))
+  logBase =
+    lift2
+      logBase
+      (\x y z' -> (- z' * log y / (x * log x ^ (2 :: Int))))
+      (\x y z' -> (+ z' / (y * log x)))
   sin = lift1 sin $ \x y' -> (+ y' * cos x)
   cos = lift1 cos $ \x y' -> (- y' * sin x)
   tan = lift1 tan $ \x y' -> (+ y' / cos x ^ (2 :: Int))
