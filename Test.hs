63df1611b0d647e7cf8a167acc6ddad719186fcd
Ryan Meredith <ryan@ryanmeredith.com>
2025-09-11 15:52:00 -0400

    New test

--- Test.hs
+++ Test.hs
@@ -3,9 +3,14 @@ module Main (main) where
 import Data.Autodiff (autodiff)
 import System.Environment (getArgs)
 
+fib :: (Num a, Ord a) => a -> a
+fib n
+  | n <= 1 = n
+  | otherwise = fib (n - 1) + fib (n - 2)
+
 main :: IO ()
 main = do
   [ns] <- getArgs
-  n <- readIO @Double ns
-  (!_, !_) <- autodiff product [1..n]
+  n <- readIO @Int ns
+  (!_, !_) <- autodiff fib n
   pure ()
